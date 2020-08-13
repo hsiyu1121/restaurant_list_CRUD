b9357bdf4c33065d6e7ddc98e5e5be794718f159
hsiyu1121 <bluefish1121@gmail.com>
2020-08-13 11:07:45 +0800

    feat: update new.hbs, edit.hbs

--- app.js
+++ app.js
@@ -29,69 +29,70 @@ db.once("open", () => {
 app.get("/", (req, res) => {
   Restaurant.find()
     .lean()
-    .then(restaurants => res.render("index", { restaurants }))
-    .catch( error => console.log(error))
+    .then((restaurants) => res.render("index", { restaurants }))
+    .catch((error) => console.log(error));
 });
 
-app.get('/restaurants/new', (req, res) => {
-  return res.render('new')
-})
+app.get("/restaurants/new", (req, res) => {
+  return res.render("new");
+});
 
-app.post('/create', (req, res) => { 
-  return Restaurant.create( req.body )
-    .then( () => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+app.post("/create", (req, res) => {
+  return Restaurant.create(req.body)
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
-app.get('/restaurants/:id', (req, res) => {
-  const id = req.params.id
+app.get("/restaurants/:id", (req, res) => {
+  const id = req.params.id;
   return Restaurant.findById(id)
     .lean()
-    .then((restaurants) => res.render('show', { restaurants }))
-    .catch(error => console.log(error))
-})
+    .then((restaurants) => res.render("show", { restaurants }))
+    .catch((error) => console.log(error));
+});
 
-app.get('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
+app.get("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
   return Restaurant.findById(id)
     .lean()
-    .then((restaurant) => res.render('edit', {restaurant}))
-    .catch(error => console.log(error))
-})
+    .then((restaurant) => res.render("edit", { restaurant }))
+    .catch((error) => console.log(error));
+});
 
-app.post('/restaurants/:id/edit', (req, res) => {
-  const id = req.params.id
+app.post("/restaurants/:id/edit", (req, res) => {
+  const id = req.params.id;
   return Restaurant.findById(id)
-    .then(restaurant => {
-     Object.assign(restaurant, req.body)
-      return restaurant.save()
+    .then((restaurant) => {
+      Object.assign(restaurant, req.body);
+      return restaurant.save();
     })
-   .then( () => res.redirect(`/restaurants/${id}`))
-   .catch(error => console.log(error))  
-})  
+    .then(() => res.redirect(`/restaurants/${id}`))
+    .catch((error) => console.log(error));
+});
 
-app.post('/restaurants/:id/delete', (req,res) => {
-  const id = req.params.id
+app.post("/restaurants/:id/delete", (req, res) => {
+  const id = req.params.id;
   return Restaurant.findById(id)
-    .then(restaurant => restaurant.remove())
-    .then(() => res.redirect('/'))
-    .catch(error => console.log(error))
-})
+    .then((restaurant) => restaurant.remove())
+    .then(() => res.redirect("/"))
+    .catch((error) => console.log(error));
+});
 
-app.get('/search', (req, res)=>{
-  console.log(req.query)
-  const keyword = req.query.keyword
-  
+app.get("/search", (req, res) => {
+  console.log(req.query);
+  const keyword = req.query.keyword;
+
   Restaurant.find()
-    .or([{name:{$regex:keyword, $options:'i'}},
-        {name_en:{$regex:keyword, $options:'i'}},
-        {location:{$regex:keyword, $options:'i'}},
-        {category:{$regex:keyword, $options:'i'}}
+    .or([
+      { name: { $regex: keyword, $options: "i" } },
+      { name_en: { $regex: keyword, $options: "i" } },
+      { location: { $regex: keyword, $options: "i" } },
+      { category: { $regex: keyword, $options: "i" } },
     ])
     .lean()
-    .then(restaurants => res.render('index', { restaurants }))
-    .catch(error => console.log(error))
-})
+    .then((restaurants) => res.render("index", { restaurants }))
+    .catch((error) => console.log(error));
+});
 
 app.listen(port, () => {
   console.log(`Express is listening on localhost:${port}`);
